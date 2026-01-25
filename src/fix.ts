56688867974adf2f8255d783fa5fe6de883f889e
tuhnps98 <tuhnps98@gmail.com>
2026-01-25 11:24:55 +0700

    Add files via upload

---
+++ src/fix.ts
@@ -0,0 +1,5 @@
+import React from 'react';
+// Ép React ra biến toàn cục ngay lập tức
+if (typeof window !== 'undefined') {
+  (window as any).React = React;
+}
---
+++ src/main.tsx
@@ -0,0 +1,12 @@
+import './fix'; // <--- QUAN TRỌNG: Nó phải nằm dòng đầu tiên!
+import React from 'react'
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.tsx'
+
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+)
