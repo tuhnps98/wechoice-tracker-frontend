354d87beb1c0164980f8caad52d276ea935a2419
tuhnps98 <tuhnps98@gmail.com>
2026-01-27 10:21:59 +0700

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,3 @@
-import './fix'; // <--- QUAN TRỌNG: Nó phải nằm dòng đầu tiên!
-import React from 'react'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
