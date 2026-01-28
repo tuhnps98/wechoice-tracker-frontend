4ca3d26c7eb7416c60d8f1065b0b6931256f3eba
tuhnps98 <tuhnps98@gmail.com>
2026-01-28 09:29:46 +0700

    Update CategorySelector.jsx

--- src/components/CategorySelector.jsx
+++ src/components/CategorySelector.jsx
@@ -1,27 +1,15 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
+import categories from "../info/categories.json";
 
 export default function CategorySelector({ onSelect }) {
-  const [categories, setCategories] = useState([]);
-  const apiUrl = import.meta.env.VITE_API_URL;
-
-  useEffect(() => {
-    if (apiUrl) {
-      axios.get(`${apiUrl}/category`)
-        .then(res => {
-          setCategories(res.data);
-        })
-        .catch(err => console.error("Lỗi tải danh mục:", err));
-    }
-  }, [apiUrl]);
-
   return (
     <select
-      className="max-w-4xl w-full px-6 py-3 bg-white border-2 border-gray-300 rounded-full shadow-sm hover:shadow-md transition-all duration-300 focus:outline-none focus:ring-4 focus:ring-gray-300 focus:border-gray-400 text-gray-700 font-medium cursor-pointer"
+      className="max-w-4xl w-full px-6 py-3 bg-white border-2 border-gray-300 rounded-full shadow-sm hover:shadow-md  transition-all duration-300 focus:outline-none focus:ring-4 focus:ring-gray-300 focus:border-gray-400 text-gray-700 font-medium cursor-pointer"
       onChange={(e) => onSelect(e.target.value)}
     >
       <option value="" className="text-gray-500">
-        -- Chọn hạng mục --
+        Chọn hạng mục để phân tích
       </option>
       {categories.map((c) => (
         <option key={c.id} value={c.id} className="py-2">
